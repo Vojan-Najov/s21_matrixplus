ec42d2aee41cf5973bd9f9955970c7cf1518fde4
Vojan-Najov <serejah.mashichev@gmail.com>
2023-06-16 02:23:59 +0300

    Add tests for accessors and mutators.

--- tests/test_s21_matix_oop.cc
+++ tests/test_s21_matix_oop.cc
@@ -22,6 +22,7 @@ TEST(MatrixConstructorDestructor, ConvertingConstructor) {
 
 	EXPECT_EQ(m.GetRows(), rows);
 	EXPECT_EQ(m.GetCols(), cols);
+
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < cols; ++j) {
 			EXPECT_EQ(m(i, j), 0.0);
@@ -34,6 +35,7 @@ TEST(MatrixConstructorDestructor, ConvertingConstructorBadRows) {
 	int cols = 87;
 
 	EXPECT_THROW(S21Matrix(rows, cols), std::invalid_argument);
+
 	rows = 0;
 	EXPECT_THROW(S21Matrix(rows, cols), std::invalid_argument);
 }
@@ -43,6 +45,7 @@ TEST(MatrixConstructorDestructor, ConvertingConstructorBadCols) {
 	int cols = -87;
 
 	EXPECT_THROW(S21Matrix(rows, cols), std::invalid_argument);
+
 	cols = 0;
 	EXPECT_THROW(S21Matrix(rows, cols), std::invalid_argument);
 }
@@ -92,6 +95,176 @@ TEST(MatrixConstructorDestructor, MoveConstructor) {
 	}
 }
 
+TEST(MatrixAccessorMutator, RowsColsAccessor) {
+	int rows = 1111;
+	int cols = 249;
+
+	S21Matrix m(rows, cols);
+	EXPECT_EQ(m.GetRows(), rows);
+	EXPECT_EQ(m.GetCols(), cols);
+}
+
+TEST(MatrixAccessorMutator, GreaterRowsMutator) {
+	int rows = 91;
+	int new_rows = 106;
+	int cols = 14;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetRows(new_rows);
+	EXPECT_EQ(m2.GetRows(), new_rows);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+	for (int i = rows; i < new_rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), 0.0);
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, LessRowsMutator) {
+	int rows = 91;
+	int new_rows = 80;
+	int cols = 14;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetRows(new_rows);
+	EXPECT_EQ(m2.GetRows(), new_rows);
+	for (int i = 0; i < new_rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, EqualRowsMutator) {
+	int rows = 91;
+	int cols = 14;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetRows(rows);
+	EXPECT_EQ(m2.GetRows(), rows);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, BadRowsMutator) {
+	int rows = 91;
+	int cols = 14;
+
+	int new_rows = 0;
+	S21Matrix m(rows, cols);
+	EXPECT_THROW(m.SetRows(new_rows), std::invalid_argument);
+	new_rows = -16;
+	EXPECT_THROW(m.SetRows(new_rows), std::invalid_argument);
+}
+
+TEST(MatrixAccessorMutator, GreaterColsMutator) {
+	int rows = 91;
+	int cols = 14;
+	int new_cols = 106;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetCols(new_cols);
+	EXPECT_EQ(m2.GetCols(), new_cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+	for (int i = 0; i < rows; ++i) {
+		for (int j = cols; j < new_cols; ++j) {
+			EXPECT_EQ(m2(i, j), 0.0);
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, LessColsMutator) {
+	int rows = 91;
+	int cols = 80;
+	int new_cols = 14;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetCols(new_cols);
+	EXPECT_EQ(m2.GetCols(), new_cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < new_cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, EqualColsMutator) {
+	int rows = 91;
+	int cols = 14;
+
+	S21Matrix m1(rows, cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			m1(i, j) = i * 1.12 + j * 2.27 + 0.99;
+		}
+	}
+
+	S21Matrix m2(m1);
+	m2.SetCols(cols);
+	EXPECT_EQ(m2.GetCols(), cols);
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			EXPECT_EQ(m2(i, j), m1(i, j)) << "i = " << i << "  j = " << j;
+		}
+	}
+}
+
+TEST(MatrixAccessorMutator, BadColsMutator) {
+	int rows = 91;
+	int cols = 14;
+
+	int new_cols = 0;
+	S21Matrix m(rows, cols);
+	EXPECT_THROW(m.SetCols(new_cols), std::invalid_argument);
+	new_cols = -16;
+	EXPECT_THROW(m.SetCols(new_cols), std::invalid_argument);
+}
 
 int main(int argc, char **argv)
 {
